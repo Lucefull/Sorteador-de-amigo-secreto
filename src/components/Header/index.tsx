bdd6c256771ce5fa016483b0ed20042802c91606
Lucefull <luansteffens@gmail.com>
2022-12-13 22:02:42 -0300

    Feat: concluido estilização do component header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,40 +1,65 @@
 import React from 'react';
 import styled from 'styled-components';
 import imgParticipante from '../../assets/participante.png';
-const Wrapper = styled.header`
-  box-sizing: border-box;
-  position: absolute;
-  width: 100vw;
+import imgLogo from '../../assets/logo.png';
+import imgSmallLogo from '../../assets/logo-pequeno.png';
 
+const Content = styled.div`
+  display: flexbox;
   background: #4b69fd;
   border: 2px solid #000000;
-`;
+  
 
-const Content = styled.main`
-  max-width: 1040px;
-  height: 454px;
-`;
+  flex-direction: column;
+  width: 100vw;
+  max-width: 400px;
+  //padding-top: 30px;
+  height: 384px;
+  justify-content: center;
 
-const Logo = styled.div`
-  background-image : url('../../assets/logo.png');
-  width: 351px;
-  height: 117px;
-`;
+  div {
+    background: url(${imgSmallLogo}) no-repeat;
+    width: 235px;
+    height: 199px;
+  }
 
-const Participante = styled.img`
-  z-index: -1;
+  img {
+    z-index: 1;
+    position: absolute;
+    width: 328px;
+    height: 158px;
+    left: 16px;
+    top: 203px;
+  }
+
+  @media screen and (min-width: 1024px) {
+    flex-direction: row;
+    width: 100vw;
+    max-width: 1024px;
+    height: 384px;
+
+    align-items: center;
+    justify-content: space-between;
+
+    div {
+      background: url(${imgLogo}) no-repeat;
+      width: 351px;
+      height: 117px;
+    }
+    img {
+      width: 450px;
+      height: 277px;
+      left: 480px;
+      top: 121px;
+    }
+  }
 `;
 
 export const Header = () => {
   return (
-    <Wrapper>
-      <Content>
-        <Logo role={'img'} aria-label={'Logo sorteador'} />
-        <Participante
-          src={imgParticipante}
-          alt="Participante com um presente na mão"
-        />
-      </Content>
-    </Wrapper>
+    <Content>
+      <div role={'img'} aria-label={'Logo sorteador'} />
+      <img src={imgParticipante} alt="Participante com um presente na mão" />
+    </Content>
   );
 };
